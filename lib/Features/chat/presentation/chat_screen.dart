aa7a09a8f4277678677f91a5646a8a2c9bc237be
ahmed abdallah <ahmedabdallah11186@gmail.com>
2024-07-31 11:11:52 +0300

    Create chat_screen.dart

---
+++ lib/Features/chat/presentation/chat_screen.dart
@@ -0,0 +1,102 @@
+import 'package:car_rentting/core/utils/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get/get.dart';
+import 'package:uuid/uuid.dart';
+import '../../auth/presentation/manager/current_user_provider.dart';
+class ChatPage extends ConsumerStatefulWidget {
+  const ChatPage({super.key});
+
+  @override
+  ConsumerState<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends ConsumerState<ChatPage> {
+  List<types.Message> _messages = [];
+  late types.User _user;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMessages();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final user = ref.watch(currentUserProvider)!;
+    _user = types.User(id: user.userId.toString(), firstName: user.name);
+  }
+
+  void _addMessage(types.Message message) {
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  void _handlePreviewDataFetched(
+      types.TextMessage message,
+      types.PreviewData previewData,
+      ) {
+    final index = _messages.indexWhere((element) => element.id == message.id);
+    final updatedMessage = (_messages[index] as types.TextMessage).copyWith(
+      previewData: previewData,
+    );
+
+    setState(() {
+      _messages[index] = updatedMessage;
+    });
+  }
+
+  void _handleSendPressed(types.PartialText message) {
+    final textMessage = types.TextMessage(
+      author: _user,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      id: const Uuid().v4(),
+      text: message.text,
+    );
+
+    _addMessage(textMessage);
+  }
+
+  void _loadMessages() async {
+    final response = await rootBundle.loadString('assets/messages.json');
+    final messages = (jsonDecode(response) as List)
+        .map((e) => types.Message.fromJson(e as Map<String, dynamic>))
+        .toList();
+
+    setState(() {
+      _messages = messages;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: Text("Chat".tr),
+    ),
+    body: SafeArea(
+      child: Chat(
+        messages: _messages,
+        // onAttachmentPressed: _handleAttachmentPressed,
+        // onMessageTap: _handleMessageTap,
+        onPreviewDataFetched: _handlePreviewDataFetched,
+        onSendPressed: _handleSendPressed,
+        theme: DefaultChatTheme(
+            backgroundColor: AppColors.white,
+            primaryColor:
+            AppColors.primaryColorSALEK1.withOpacity(.8),
+            secondaryColor: AppColors.primaryColor,
+            inputBackgroundColor: AppColors.lightColor2),
+        showUserAvatars: true,
+        showUserNames: true,
+        user: _user,
+      ),
+    ),
+  );
+}
+
