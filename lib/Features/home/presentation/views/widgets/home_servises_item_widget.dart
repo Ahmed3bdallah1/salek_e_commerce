ebd15e7fdd0971f286337186c62cc1ccba938484
ahmed abdallah <ahmedabdallah11186@gmail.com>
2024-08-23 23:12:15 +0300

    Update home_servises_item_widget.dart

--- lib/Features/home/presentation/views/widgets/home_servises_item_widget.dart
+++ lib/Features/home/presentation/views/widgets/home_servises_item_widget.dart
@@ -26,41 +26,49 @@ class HomeServicesItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100.h,
+      height: 160.h,
       margin: const EdgeInsetsDirectional.only(start: 12, end: 12),
-      decoration:
-          BoxDecoration(borderRadius: BorderRadius.circular(5), color: color),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12.r),
+        color: color,
+      ),
       child: InkWell(
         onTap: () {
-          Get.to(() => AllCategories(
-category.catNameAr,
-            id: category.catId));
+          Get.to(() => AllCategories(category.catNameAr, id: category.catId));
         },
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(
-              height: 10.h,
-            ),
-            ImageOrSvg(height: 80.h, width: 100.w, imageUrl),
-            Container(
-              decoration: BoxDecoration(
-                  color: AppColors.white.withOpacity(.7),
-                  borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(8),
-                      topRight: Radius.circular(8))),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 12),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      title,
-                      style: AppFontStyle.black_18.copyWith(
-                          fontWeight: FontWeight.bold, color: textColor),
-                    ),
-                  ],
+            SizedBox(height: 10.h),
+            ImageOrSvg(height: 90.h, imageUrl),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 5.h),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: AppColors.white.withOpacity(.7),
+                    borderRadius: BorderRadius.circular(12.r),
+                    border: Border.all(color: AppColors.white)),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        category.catNameAr,
+                        style: AppFontStyle.black_18.copyWith(
+                            fontWeight: FontWeight.bold, color: textColor),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        category.catNameEn,
+                        style: AppFontStyle.black_18.copyWith(
+                            fontWeight: FontWeight.bold, color: textColor),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -77,35 +85,41 @@ class HomeServicesItemShimmer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100.h,
+      height: 160.h,
       margin: const EdgeInsetsDirectional.only(start: 12, end: 12),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(5),
-        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(12.r),
+        color: AppColors.primaryColor3,
       ),
       child: Shimmer.fromColors(
-        baseColor: Colors.grey[400]!,
-        highlightColor: Colors.grey[100]!,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Container(
-              height: 80.h,
-              width: 100.w,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(5),
+        baseColor: AppColors.primaryColor4.withOpacity(.3),
+        highlightColor: AppColors.primaryColor4.withOpacity(.9),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                height: 90.h,
+                width: 200.w,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12.r),
+                ),
               ),
-            ),
-            Container(
-              width: 70.w,
-              height: 16.h,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(5),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: Container(
+                  width: double.infinity,
+                  height: 46.h,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
